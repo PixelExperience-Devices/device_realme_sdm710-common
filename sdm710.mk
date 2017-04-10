762b7c90db6f084f9a9a6a89882056ab0f6c9025
Ruthwar Kumar Ambeer <ruthwar@codeaurora.org>
2017-04-10 15:41:38 +0530

    sdm710-common: Add background atel services in whitelist
    telephony services are white-listed who all need
    to freely run in the background. This is temporary
    work-around and some of the services will be
    removed from the list based on the feasibility.
    
    Change-Id: Ice11429b5b78d5786dc701251b507b7dac5745ee

--- sdm710.mk
+++ sdm710.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# QTI
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
