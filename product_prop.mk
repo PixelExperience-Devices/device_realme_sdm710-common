fdbc6170062bfbe45b697fdd42e3855b4348ef4e
Luca Stefani <luca.stefani.ge1@gmail.com>
2018-08-11 10:19:13 +0200

    sdm710-common: Enable subsystem restart
    * This is a failsafe to recover critical services
      running on the hexagon CPU ( eg. modem )
    
    Change-Id: Ib54c39fb9d66d8498f68d64666a2d345b9252f1c
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -66,3 +66,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# SSR
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.ssr.restart_level=ALL_ENABLE
