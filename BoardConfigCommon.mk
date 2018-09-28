c92f75eac0e1a49f2783846b040abee8eea072e6
GuaiYiHu <guaiyihu@foxmail.com>
2018-09-28 13:52:17 +0800

    sdm710-common: wifi: Enable WIFI_HIDL_FEATURE_DUAL_INTERFACE
    See from https://source.android.com/devices/tech/connect/wifi-sta-ap-concurrency
    
    Change-Id: I64f9e7b76e6dffaf59cd78eae4ba3e75f3b13808
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -177,6 +177,7 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 WIFI_DRIVER_OPERSTATE_PATH := "/sys/class/net/wlan0/operstate"
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 
 # Inherit from the proprietary version
 #-include vendor/realme/sdm710-common/BoardConfigVendor.mk
