f02b2268c8556ff2a23a07ee6ea883aa2a932d34
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    sdm710-common: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -83,7 +83,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.2-impl \
     audio.primary.sdm710 \
     audio.a2dp.default \
     audio.r_submix.default \
