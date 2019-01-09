272655580a81b99b44822c1001108fb79bc45b68
Joey <joey@lineageos.org>
2019-01-09 22:24:07 +0100

    sdm710-common: Build Trust HAL (system variant)
    Change-Id: I2a426b2c2dda70f41e3c3ea6f85905b0daf65f0d
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -100,6 +100,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
