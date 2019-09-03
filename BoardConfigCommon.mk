f88111e8916246908788ebef186dbb8ccaaf2a6c
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-03 13:52:52 +0000

    sdm710-common: Define FORCE_QCOM_DISPLAY_HAL_VARIANT
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,9 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a75
 TARGET_BOOTLOADER_BOARD_NAME := sdm710
 TARGET_NO_BOOTLOADER := true
 
+# Platform
+FORCE_QCOM_DISPLAY_HAL_VARIANT := sdm845
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=16
 BOARD_KERNEL_BASE := 0x00000000
