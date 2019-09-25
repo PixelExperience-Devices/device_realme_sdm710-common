e7463048b63dbd37b81eaeb122b4620d188e11c4
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    sdm710-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -87,6 +87,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.disable_ubwc=0
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
+    ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
 
