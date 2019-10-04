8b29969b034f8e763ae14d132178b8396a2a3b71
SagarMakhar <sagarmakhar@gmail.com>
2019-10-04 10:35:02 +0000

    sdm710-common: Copy proximity and light sensor permissions to system
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -92,6 +92,11 @@ PRODUCT_SOONG_NAMESPACES += \
 # System properties
 -include $(LOCAL_PATH)/system_prop.mk
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+
 # Telephony
 PRODUCT_PACKAGES += \
     telephony-ext
