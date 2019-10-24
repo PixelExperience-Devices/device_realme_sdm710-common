f0a501241d1d7145bfb0d6e5417989abced3285f
SagarMakhar <sagarmakhar@gmail.com>
2019-10-24 15:32:00 +0000

    sdm710-common: Build thermal hal
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -321,6 +321,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.sdm710
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
