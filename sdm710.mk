0a694724bd0c1fa04158e37e5c0785aadb6b80ea
SagarMakhar <sagarmakhar@gmail.com>
2019-10-24 14:30:29 +0000

    sdm710-common: Build camera packages
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -134,6 +134,10 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    libxml2 \
     Snap
 
 # CNE
