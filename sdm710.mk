7c04493115a4008c71386df4596b95f729576037
SagarMakhar <sagarmakhar@gmail.com>
2019-10-24 15:23:21 +0000

    sdm710-common: Build USB and vibrator hals
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -311,6 +311,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service \
+
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-service \
+    android.hardware.vibrator@1.0-impl
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
