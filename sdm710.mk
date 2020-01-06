0ab93b92fd3000537feea5aeff8505aa42c3d472
SagarMakhar <sagarmakhar@gmail.com>
2020-01-06 15:02:38 +0530

    sdm710-common: Add perf jars to bootclasspath
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -319,6 +319,11 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
+# Perf
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    UxPerformance
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
