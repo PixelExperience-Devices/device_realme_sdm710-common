6318faba99c20f2dc72849a4fb636b7a8d4ad0cc
SamarV-121 <samarvispute121@gmail.com>
2020-01-30 12:28:03 +0530

    sdm710-common: Import some radio props
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -52,7 +52,12 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # RIL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.telephony.default_network=22,18 \
-    persist.radio.add_power_save=1
+    persist.radio.add_power_save=1 \
+    persist.vendor.radio.lte_vrte_ltd=1 \
+    persist.vendor.radio.cs_srv_type=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.force_on_dc=true \
+    persist.vendor.radio.redir_party_num=1
 
 # UI
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
