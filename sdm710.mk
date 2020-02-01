49793fe0fd8216425354e517332c562f7a440f2b
SagarMakhar <sagarmakhar@gmail.com>
2020-02-02 01:42:56 +0530

    sdm710-common: Don't enforce RRO on all targets
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -19,11 +19,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
-
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_props.mk
 
