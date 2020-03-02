715148fe2fd6c1413510f0bad5b463d444e01b87
SamarV-121 <samarvispute121@gmail.com>
2020-03-02 15:30:04 +0530

    sdm710-common: Remove unused qti-telephony-common
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -107,13 +107,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
