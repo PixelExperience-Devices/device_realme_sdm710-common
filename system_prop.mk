d18d0798ba07db219ff55b3957dfccd2693fb860
SamarV-121 <samarvispute121@gmail.com>
2020-03-02 15:46:54 +0530

    sdm710-common: Cleanup system props
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- system_prop.mk
+++ system_prop.mk
@@ -1,10 +1,3 @@
-  
-# Assertive display
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-   ro.vendor.display.ad=1 \
-   ro.vendor.display.sensortype=2 \
-   ro.vendor.display.ad.sdr_calib_data=/vendor/etc/OPPO_OLED_AD_calib.cfg
-
 # Audio
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    ro.config.vc_call_vol_steps=7 \
@@ -14,14 +7,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
    vendor.bluetooth.soc=cherokee
 
-# Camera
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-   persist.camera.HAL3.enabled=1
-
 # Data Modules
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.vendor.data.mode=concurrent \
-    ro.vendor.use_data_netmgrd=true
+    persist.vendor.data.mode=concurrent
 
 # FM
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -30,8 +18,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Graphics
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.enable_hwc_vds=1 \
-    debug.sf.enable_gl_backpressure=1 \
-    debug.sf.early_phase_offset_ns=5000000
+    debug.sf.enable_gl_backpressure=1
 
 # Media
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -41,18 +28,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.netflix.bsp_rev=Q670-14477-1
 
-# Memory optimizations
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.vendor.qti.sys.fw.bservice_enable=true
-
-# Perf
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    vendor.iop.enable_prefetch_ofr=0 \
-    vendor.iop.enable_uxe=0
-
 # RIL
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.telephony.default_network=22,18 \
+    ro.telephony.default_network=22,20 \
     persist.radio.add_power_save=1 \
     persist.vendor.radio.lte_vrte_ltd=1 \
     persist.vendor.radio.cs_srv_type=1 \
