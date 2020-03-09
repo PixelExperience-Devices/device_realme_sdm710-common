82bbf4d6387e8e4afc0e02e1b6e6913e734d1844
SamarV-121 <samarvispute121@gmail.com>
2020-03-09 17:25:32 +0530

    sdm710-common: usb: Convert to realme_sdm710 targets
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.realme_sdm710
 
 # WiFi
 PRODUCT_COPY_FILES += \
