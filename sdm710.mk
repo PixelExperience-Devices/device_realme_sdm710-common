6cfd619964cbbfef30e0a0b9bbcb2efe9d1382fa
SamarV-121 <samarvispute121@gmail.com>
2020-03-09 20:57:36 +0530

    Revert "sdm710-common: Build libldacBT_dec"
    This reverts commit c3085e77c986df093962e3e8462246e87c2fca72.
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -30,10 +30,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    libldacBT_dec
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap
