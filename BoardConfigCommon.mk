879aa5df819ef84feaa7fe547b993d019ed6731c
SamarV-121 <samarvispute121@gmail.com>
2020-03-18 23:19:26 +0530

    sdm710-common: sepolicy: Address selinux denials
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -86,13 +86,13 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 # Sepolicy
 # PRIVATE_EXCLUDE_BUILD_TEST := true
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
-# BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-#     device/qcom/sepolicy/generic/private \
-#     device/qcom/sepolicy/qva/private
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/private \
+    device/qcom/sepolicy/qva/private
 
-# BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-#     device/qcom/sepolicy/generic/public \
-#     device/qcom/sepolicy/qva/public
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/qcom/sepolicy/generic/public \
+    device/qcom/sepolicy/qva/public
 
 # Treble
 BOARD_VNDK_VERSION := current
