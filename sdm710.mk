e333f846007cbc2d415a51aa55cca6f55b846659
SamarV-121 <samarvispute121@gmail.com>
2020-03-20 10:13:06 +0530

    sdm710-common: Compile libvulkan & vendor.display.config@1.0
     * were removed in c4eef6e ;__;
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -52,6 +52,11 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI \
     NexusLauncherRelease
 
+# Display
+PRODUCT_PACKAGES += \
+    libvulkan \
+    vendor.display.config@1.0
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710
