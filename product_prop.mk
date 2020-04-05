5788e2f923c9d912958c458bf5544afc0f9295d1
Sᴀᴍᴀʀ Vɪꜱᴘᴜᴛᴇ <samarvispute121@gmail.com>
2020-04-05 22:51:22 +0530

    Revert "sdm710-common: Override some radio props"
    mah bad 
    This reverts commit 9316bb9f6ecdf51dec97316532c82489e67dc3ac.
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -54,6 +54,4 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.radio.cs_srv_type=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.force_on_dc=true \
-    persist.vendor.radio.redir_party_num=1 \
-    persist.vendor.radio.relay_oprt_change=0 \
-    persist.vendor.radio.poweron_opt=0
+    persist.vendor.radio.redir_party_num=1
