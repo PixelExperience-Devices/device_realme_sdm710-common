3a020d18eaf06f55959660fce083c47ff0ec2de6
Purushottam Kushwaha <pkushwah@codeaurora.org>
2020-05-06 19:57:38 +0530

    sdm710-common: NAN: set property 'wifi.aware.interface' to use separate interface
    This CL is to set 'wifi.aware.interface' property for the targets
    where separate NAN discovery interface is supported.
    
    Change-Id: Ib892f2207a9366a5a86071fd6d3c4b37e174b08c
    CRs-Fixed: 2680050
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -181,3 +181,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1
+
+# Wi-Fi
+wifi.aware.interface=wifi-aware0
