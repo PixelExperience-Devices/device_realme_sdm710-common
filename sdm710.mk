4a5b4d96c49a2655c192db0d01c1e5019749f385
SamarV-121 <samarvispute121@gmail.com>
2020-05-13 17:04:54 +0530

    sdm710-common: Disable IOP and PERF HAL
     * also revert 56f6504f61e1c7e6bb0e74278d215da0469cf4a3, eb7a690439298070393a5fe8c291014e6615eaec, d5cb5501690c3d949a342ac203ba5544a5768106
     * in the favour of power-libperfmgr
    
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -98,14 +98,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
-# QTI Performance
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perfconfigstore.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfconfigstore.xml
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
