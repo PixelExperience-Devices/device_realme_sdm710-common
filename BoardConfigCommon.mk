d449ae81dcd383625bc0df094a51e5f9e2a5eeff
ѕαмαя νιѕρυтє <samarvispute121@gmail.com>
2020-06-13 16:11:17 +0530

    [REVERT] sdm710-common: build have overriding commands
    ccc7d08541abd6e9358aea3a97b5e404908aab07

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,9 +25,6 @@ TARGET_NO_BOOTLOADER := true
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := sdm710
 
-# Build System
-BUILD_BROKEN_DUP_RULES := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=16
 BOARD_KERNEL_BASE := 0x00000000
