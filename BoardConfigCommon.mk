e26ff8402286a3408cbb623c15db161c7aa38885
Bruno Martins <bgcngm@gmail.com>
2020-08-26 14:56:35 +0000

    sdm710-common: Create manifest file with the common interfaces
    Change-Id: I1c4529f48c5b0c3f7a10a167afd033e070131bae
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -117,6 +117,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
 # Keystore
 TARGET_PROVIDES_KEYMASTER := true
