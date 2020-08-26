6cf36e90fd682f08716038fe8b91bedda1898549
SagarMakhar <sagarmakhar@gmail.com>
2020-08-26 16:34:16 +0000

    sdm710-common: Build 64-bit version of Camera Provider
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -134,7 +134,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     Snap
 
