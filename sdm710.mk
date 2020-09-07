e5c1f66df5afc9bb435568c92297f973072d27ca
SagarMakhar <sagarmakhar@gmail.com>
2020-09-07 05:03:55 +0000

    sdm710-common: Remove perf libs from bootjars
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -289,11 +289,6 @@ PRODUCT_PACKAGES += \
     libOmxVidcCommon \
     libstagefrighthw
 
-# Perf
-PRODUCT_BOOT_JARS += \
-    QPerformance \
-    UxPerformance
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
