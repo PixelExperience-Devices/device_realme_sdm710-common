b1f8bbae4b164d594bafddb248f4e2d147d5cb95
Redick Lin <redicklin@google.com>
2020-09-19 18:02:36 +0530

    sdm710-common: Use phase offsets as duration
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    Bug: 158464992
    Test: scrolling, app launching/closing, playing video
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -141,11 +141,16 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.gralloc.disable_ubwc=0
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.use_color_management=true \
@@ -187,9 +192,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.sib16_support=1
 
 # Wi-Fi
-wifi.aware.interface=wifi-aware0
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.aware.interface=wifi-aware0
 
 # ZRAM writeback
-ro.zram.mark_idle_delay_mins=60
-ro.zram.first_wb_delay_mins=180
-ro.zram.periodic_wb_delay_hours=24
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.zram.mark_idle_delay_mins=60 \
+    ro.zram.first_wb_delay_mins=180 \
+    ro.zram.periodic_wb_delay_hours=24
