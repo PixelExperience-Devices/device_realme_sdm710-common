1f9a8388510801b3f573893fdd2ba1cab1cb0c5b
Henrique Silva <jhsv09@gmail.com>
2021-01-30 22:29:43 +0000

    [DNM] sdm710-common: Introduce 'SafailNet'
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -267,3 +267,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/fix_ex_sdcard.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
+
+#Saifailnet
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
