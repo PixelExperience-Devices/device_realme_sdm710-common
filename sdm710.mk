d256b52130ab5416cc01f783e934e5390139681d
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-03-10 05:59:11 +0000

    Revert "sdm710-common: Add updatable_apex.mk"
    This reverts commit a4c6db2f83f2bc488b65ca239c1ade2f39dac374.

--- sdm710.mk
+++ sdm710.mk
@@ -11,9 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/realme/sdm710-common/sdm710-common-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
