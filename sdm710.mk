8b7ec648970ed119f9f54f8a2e8c912433935e2d
Arian <arian.kulmer@web.de>
2021-04-01 01:09:38 +0200

    sdm710-common: Remove xtwfi-client and xtwifi-inet-agent
    Change-Id: I232fd3b351f0e47b86f7d5d7b2a2bd0f28712d64
    Signed-off-by: AkaruiAikara <vall@aikara.me>

--- sdm710.mk
+++ sdm710.mk
@@ -237,8 +237,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
     $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
     $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf
 
 # Health
 PRODUCT_PACKAGES += \
