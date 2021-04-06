369e9f64bc5796f23bf9b24e130f78a780550bf1
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-04-06 15:42:26 +0530

    Revert "RMX1971: Add kcal to KharaMeParts"
    This reverts commit dbaebe936dfd9a9e39b10dd35554e6cfded6cb56.
    
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- KharaMeParts/src/com/kharame/kharameparts/KharaMeParts.java
+++ KharaMeParts/src/com/kharame/kharameparts/KharaMeParts.java
@@ -30,7 +30,6 @@ import androidx.preference.TwoStatePreference;
 
 import com.kharame.kharameparts.settings.ScreenOffGestureSettings;
 import com.kharame.kharameparts.doze.DozeSettingsActivity;
-import com.kharame.kharameparts.kcal.DisplayCalibration;
 import com.kharame.kharameparts.vibration.VibratorStrengthPreference;
 
 import java.text.DateFormat;
@@ -54,7 +53,6 @@ public class KharaMeParts extends PreferenceFragment implements
     private Preference mGesturesPref;
     private Context mContext;
     private SharedPreferences mPreferences;
-    private Preference mKcalPref;
     private VibratorStrengthPreference mVibratorStrength;
 
     @Override
@@ -82,16 +80,6 @@ public class KharaMeParts extends PreferenceFragment implements
         mVibratorStrength = (VibratorStrengthPreference) findPreference(VibratorStrengthPreference.KEY_VIBSTRENGTH);
         mVibratorStrength.setEnabled(VibratorStrengthPreference.isSupported());
     }
-
-	mKcalPref = findPreference("kcal");
-                mKcalPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                     @Override
-                     public boolean onPreferenceClick(Preference preference) {
-                         Intent intent = new Intent(getContext(), DisplayCalibration.class);
-                         startActivity(intent);
-                         return true;
-                     }
-                });
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
