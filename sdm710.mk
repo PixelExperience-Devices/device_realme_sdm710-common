c0d2b5b4a3ae42054588cbfb46cc87244d254fcb
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-04-22 19:13:10 +0530

    sdm710-common: Drop bitness from camera provider
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -136,7 +136,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl:32 \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     libxml2 \
     Snap
