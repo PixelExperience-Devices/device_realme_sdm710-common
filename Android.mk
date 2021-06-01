96708cb549de2e0d2b2d5f73344bfdc6ef06e905
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-06-01 18:17:37 +0530

    sdm710-common: Add RMX1851

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter RMX1901 RMX1921 RMX1971,$(TARGET_DEVICE)),)
+ifneq ($(filter RMX1901 RMX1921 RMX1971 RMX1851,$(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
