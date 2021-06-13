5b84f64fd05a23183230dac8de96d1d64454e54b
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-06-13 02:31:27 +0000

    Revert "sdm710-common: Migrate to Health 2.1 HAL"
    This reverts commit d7da86b62e6b86f8de29326a844ad017e22b3fe0.
    
    * Thank you Realme
    
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -240,9 +240,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
