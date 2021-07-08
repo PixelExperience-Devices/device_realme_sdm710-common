0b382c8235b57735a702289c8a031a76e351d8ac
Chris Fries <cfries@google.com>
2021-07-08 10:33:18 +0200

    sdm710-common: Set VENDOR_SECURITY_PATCH to platform
    Set the VENDOR_SECURITY_PATCH level to match the platform
    security patch level.  These are functionally the same.
    
    Bug: 125914443
    Change-Id: Ib77ab115d6faabd21958c34e2dae49ec96421fdd
    Signed-off-by: AkaruiAikara <vall@aikara.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -161,7 +161,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-08-05
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
