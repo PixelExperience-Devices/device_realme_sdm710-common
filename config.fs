fccdf47786629cbfe25ac24891f72d83a8490898
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-10-09 07:53:41 +0000

    sdm710-common: Revert config.fs changes

--- config.fs
+++ config.fs
@@ -26,7 +26,7 @@ caps: BLOCK_SUSPEND
 mode: 0755
 user: AID_BLUETOOTH
 group: AID_BLUETOOTH
-caps: BLOCK_SUSPEND NET_ADMIN SYS_NICE
+caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
