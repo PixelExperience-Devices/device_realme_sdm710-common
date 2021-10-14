0134d717c65eac3de8b08066f03be63c9647dd94
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-10-14 18:53:32 +0530

    sdm710-common: Update sepolicy flags for android 12

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -168,7 +168,7 @@ VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 
 # Treble
 BOARD_VNDK_VERSION := current
