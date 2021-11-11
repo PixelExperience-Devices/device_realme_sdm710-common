fc34a533dd82ba25a2743d98d7d81d3917c7d9c5
MadhavSaladi <baaswanthmadhav@gmail.com>
2021-11-11 05:07:02 +0000

    Revert "[DNM] sdm710-common: Introduce 'SafailNet'"
    This reverts commit 1f9a8388510801b3f573893fdd2ba1cab1cb0c5b.
    
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -259,12 +259,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/apex_metadata.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT)/etc/init
 include $(BUILD_PREBUILT)
-
-#Saifailnet
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.safailnet.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.safailnet.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
