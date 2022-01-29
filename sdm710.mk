800bed99d91a3247bab9de1a4cf6cec61590c573
MadhavSaladi <baaswanthmadhav@gmail.com>
2022-01-29 12:28:48 +0000

    sdm710-common: Switch to AOSP WFD

--- sdm710.mk
+++ sdm710.mk
@@ -456,14 +456,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-#PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_PACKAGES += \
     android.frameworks.automotive.display@1.0.vendor \
     android.frameworks.cameraservice.common@2.0.vendor \
