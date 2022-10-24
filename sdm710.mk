8bc84c5db4d2bcf9e4673ab26150a58f11e9b145
kssrao13882 <kssrao13882@gmail.com>
2022-10-24 17:17:23 +0530

    sdm710-common: Nuke GcamGo
     - Let's use Cam shipped with Rom Source
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -148,8 +148,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
-    libxml2 \
-    GcamGo
+    libxml2
 
 # CNE
 PRODUCT_PACKAGES += \
