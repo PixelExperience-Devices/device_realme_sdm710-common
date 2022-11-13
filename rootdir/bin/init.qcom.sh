3f8fe8c0a8f58a0f343c0d8ff4d11a7a63b3e7d4
EmanuelCN <emanuelghub@gmail.com>
2022-11-13 18:07:42 +0530

    sdm710-common: rootdir: remove resource cache too on early boot
     - Android won't do it on a dirty flash, so certain elements will be broken if something is changed in UI
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- rootdir/bin/init.qcom.sh
+++ rootdir/bin/init.qcom.sh
@@ -451,6 +451,7 @@ chmod g-w /data/vendor/modem_config
 setprop ro.vendor.ril.mbn_copy_completed 1
 
 # Remove settings cache, avoids derps after dirty flash
+rm /data/resource-cache
 rm -rf /data/system/package_cache
 
 #check build variant for printk logging
