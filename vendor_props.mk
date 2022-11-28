5d75f2fe2eb92b7ac938084f52ccf7029dae60a1
holio0 <>
2022-11-28 09:10:49 +0530

    sdm710-common: Enable zygote critical window
    Bug: 141948707
    Change-Id: I5dc57312118b0f6d95fc9fe164363ec7e15f1b51
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -200,3 +200,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
+
+# Zygote
+zygote.critical_window.minute=10
