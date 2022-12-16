13cc4ecad3bdfd2f0e80220d7970fbea1e922418
ZVNexus <akoskovich@pm.me>
2022-12-16 21:12:45 +0530

    sdm710-common: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,8 +56,7 @@ BOARD_KERNEL_CMDLINE := \
     lpm_levels.sleep_disabled=1 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
-    swiotlb=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    swiotlb=1
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
