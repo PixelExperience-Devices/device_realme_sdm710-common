859d5f8ac18af95050e5b7d1928ac168c7979d81
Reinazhard <reinazhard@gmail.com>
2023-01-03 07:43:46 +0530

    sdm710-common: set NUM_FRAMEBUFFER_SURFACE_BUFFERS
     - Kanged from CAF product-board
    - This also fixed triple frame buffer regression
    - Nuke triple frame buffer from prop as it causing regression
    - Also nuked duplicate declaration at system.prop
    
    Signed-off-by: Reinazhard <reinazhard@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>
    Signed-off-by: OmarAlCoptan <omarcoptan9@gmail.com>
    Signed-off-by: Cykeek <Cykeek@proton.me>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -114,7 +114,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.earlyGl.sf.duration=13500000 \
     debug.sf.earlyGl.app.duration=21000000 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.has_HDR_display=true \
