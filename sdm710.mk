19ecb7c9000742757441c84548a17015fe6a7c7d
Alexander Koskovich <akoskovich@pm.me>
2023-05-01 10:32:01 +0000

    sdm710-common: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Signed-off-by: IGaganpreetSingh <gaganpannu83@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -392,10 +392,6 @@ PRODUCT_PACKAGES += \
    libtflite \
    libtextclassifier_hash
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
