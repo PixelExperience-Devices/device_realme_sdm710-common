22cc3f847f80ee951a1842d599063f267caf1e08
AMouri <alec.mouri@gmail.com>
2023-05-21 11:31:14 +0530

    sdm710-common: Enable SurfaceFlinger Cache by default
    Bug: 167936510
    Test: Multiple fixes for flickering issues have landed.
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -132,6 +132,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.enable_layer_caching=true \
     ro.surface_flinger.wcg_composition_dataspace=143261696
 
 # IMS
