f88a49ac0243eff0b2a0350c7cca384072b1070e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-06-01 12:28:42 +0530

    sdm710-common: fingerprint: Drop LOG_VERBOSE
    * It's useless since it's the same as LOG_TAG
    
    Change-Id: I1a15ea85f840b7af6d41b59cf01b0ec3c2457705
    Signed-off-by: AnilShebin <anilshebin@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710"
 
 #include <hardware/hardware.h>
 #include <hardware/fingerprint.h>
