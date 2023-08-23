4607da324aed721c9f9229dc42091d368444ea60
Vishalcj17 <vishalcj@aospa.co>
2023-08-23 05:38:15 +0530

    sdm710-common: Do not use phase offsets as duration
     - SurfaceFlinger Durations work fine in some scenarios like during
    fling, browsing on chrome and etc. But it breaks HWUI graph during
    activities like scrolling or JankBench tests, even when the the
    governor is set to performance.
    
    Test- Open UIBench.apk and look for jitter
    
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- vendor_props.mk
+++ vendor_props.mk
@@ -125,13 +125,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.egl=adreno
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=20500000 \
-    debug.sf.early.sf.duration=16000000 \
-    debug.sf.early.app.duration=16500000 \
-    debug.sf.earlyGl.sf.duration=13500000 \
-    debug.sf.earlyGl.app.duration=21000000 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.has_wide_color_display=true \
